ea2f9e3cfc6f31b8c8c1792a74dd95ebf997ec32
Scott Bellware <sbellware@gmail.com>
2015-05-04 23:47:26 -0500

    Attributes are read-only

--- lib/schema/schema.rb
+++ lib/schema/schema.rb
@@ -1,3 +1,7 @@
 module Schema
   include Virtus.module(:constructor => false, :mass_assignment => false)
+
+  def attributes
+    attribute_set.get(self)
+  end
 end
--- test/spec/schema.rb
+++ test/spec/schema.rb
@@ -3,18 +3,43 @@ require_relative 'spec_init'
 module Test
   module Schema
     def self.example
-      Example.new
+      example = Example.new
+      example.some_attribute = 'some value'
+      example
+    end
+
+    def self.ancestors
+      example.class.ancestors
+    end
+
+    def self.attributes
+      example.attributes
     end
 
     class Example
       include ::Schema
+
+      attribute :some_attribute
     end
   end
 end
 
 describe "Schema" do
-  it "Is a Virtus object" do
-    ancestors = Test::Schema.example.class.ancestors
+  it "is a Virtus object" do
+    ancestors = Test::Schema.ancestors
     assert(ancestors.include? Virtus::Model::Core)
+  end
+
+  it "has attributes" do
+    example = Test::Schema.example
+    assert(example.attributes == { some_attribute: 'some value' })
+  end
+
+  it "cannot be assigned attributes" do
+    example = Test::Schema.example
+
+    assert_raises NoMethodError do
+      example.attributes = :something
+    end
   end
 end
