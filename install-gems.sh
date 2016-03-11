68af75d693921c82f19164cbe0ead3327e923665
Nathan Ladd <nathan@obsidianexchange.com>
2016-03-11 09:47:31 -0600

    install-gems.sh is updated with the latest version

--- install-gems.sh
+++ install-gems.sh
@@ -24,19 +24,32 @@ if [ -z ${POSTURE+x} ]; then
   POSTURE="operational"
 fi
 
+scheme="https:"
+gem_repo_authority_path="gem.fury.io/obsidian/"
+public_gem_repo_uri="$scheme//$gem_repo_authority_path"
+
+if [ -n "$OBSIDIAN_GEMFURY_SECRET" ]; then
+  private_gem_repo_uri="$scheme//$OBSIDIAN_GEMFURY_SECRET@$gem_repo_authority_path"
+  private_source="--source $private_gem_repo_uri"
+fi
+
+public_source="--source $public_gem_repo_uri"
+
+ruby_gems_source="--source https://rubygems.org"
+
 echo
 echo "Installing gems locally (POSTURE: $POSTURE)"
 echo '- - -'
 for gem in *.gem; do
   echo "($gem)"
-  cmd="gem install $gem --source https://gem.fury.io/obsidian/ --install-dir ./gems"
+  cmd="gem install $gem --clear-sources $private_source $public_source $ruby_gems_source --install-dir ./gems"
 
   if [ operational != "$POSTURE" ]; then
     cmd="$cmd --development"
   fi
 
   echo $cmd
-  ($cmd)
+  ($cmd) || exit 1
 done
 
 echo '= = ='
