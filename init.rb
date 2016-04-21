61787ea5eb8f4aedc4b909f167e5ac97098e1a88
Scott Bellware <sbellware@gmail.com>
2016-04-21 18:22:38 -0500

    Package version set to 0.1.0.0

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 lib_dir = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
 
-libraries_dir = ENV['LIBRARIES_DIR']
+libraries_dir = ENV['LIBRARIES_HOME']
 unless libraries_dir.nil?
   libraries_dir = File.expand_path(libraries_dir)
   $LOAD_PATH.unshift libraries_dir unless $LOAD_PATH.include?(libraries_dir)
