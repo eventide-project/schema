eef40259a3c5b09b30f8268ec81b55f715fabf11
Scott Bellware <sbellware@gmail.com>
2016-05-05 16:33:57 -0500

    Attribute is defined by attribute macro (Virtus is removed)

--- lib/schema/schema.rb
+++ lib/schema/schema.rb
@@ -1,15 +1,35 @@
 module Schema
-  include Virtus.module(:constructor => false, :mass_assignment => false)
-
-  def initialize
-    set_default_attributes
+  def self.included(cls)
+    cls.class_exec do
+      extend AttributeMacro
+    end
   end
 
+    # def self.define(target_class, attr_name, interface=nil)
+    #   ::Attribute::Define.(target_class, attr_name, :accessor) do
+    #     Substitute.build interface
+    #   end
+    # end
+
+  module AttributeMacro
+    def attribute_macro(attr_name, interface=nil)
+      ::Attribute::Define.(self, attr_name, :accessor)
+    end
+    alias :attribute :attribute_macro
+  end
+
+
+  # include Virtus.module(:constructor => false, :mass_assignment => false)
+
+  # def initialize
+  #   set_default_attributes
+  # end
+
   def attributes
-    attribute_set.get(self)
+    # attribute_set.get(self)
   end
 
   def to_h
-    attributes
+    # attributes
   end
 end
