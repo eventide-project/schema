74d09f7bbb7d695b2f512800a9437825633d8c06
Scott Bellware <sbellware@gmail.com>
2016-05-04 20:41:34 -0500

    Tests are organized according to test-bench standard

--- test/bench.rb
+++ test/bench.rb
@@ -1,6 +1,6 @@
 require_relative 'test_init'
 
 TestBench::Runner.(
-  'spec/**/*.rb',
+  'bench/**/*.rb',
   exclude_pattern: %r{/skip\.|(?:_init\.rb|\.sketch\.rb|_sketch\.rb|\.skip\.rb)\z}
 ) or exit 1
