05061b10658fb8b4bfa36f719691123f358da055
Scott Bellware <sbellware@gmail.com>
2016-05-05 20:56:35 -0500

    Strict typed attribute must be assigned instance of exactly the attribute's declared type

---
+++ test/bench/typed_attribute/strict.rb
@@ -0,0 +1,27 @@
+require_relative '../bench_init'
+
+context "Attribute Definition (Not Strict)" do
+  example = Schema::Controls::Schema::Typed::Example.new
+
+  context "Attribute value is not of the same type as the attribute's declared interface" do
+    test "Incorrect" do
+      assert proc { example.some_attribute = 'some value' } do
+        raises_error? Schema::TypeError
+      end
+    end
+  end
+
+  context "Attribute value is of the same type as the attribute's declared interface" do
+    test "Correct" do
+      something = Schema::Controls::Schema::Typed::SomeType.new
+      example.some_attribute = something
+    end
+  end
+
+  context "Attribute value is of a subtype of the attribute's declared interface" do
+    test "Correct" do
+      something = Schema::Controls::Schema::Typed::SomeSubtype.new
+      example.some_attribute = something
+    end
+  end
+end
