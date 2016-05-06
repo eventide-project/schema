67ac7f9285bfddc28034489499a9f72b49ac00b8
Scott Bellware <sbellware@gmail.com>
2016-05-06 01:01:21 -0400

    Duplication in test is reduced

--- test/bench/schema/class_attributes_list.rb
+++ test/bench/schema/class_attributes_list.rb
@@ -1,28 +1,31 @@
 require_relative '../bench_init'
 
 context "Class Attributes List" do
-  context "Not Typed" do
-    context "Not Strict" do
-      attributes = Schema::Controls::Schema::Example.attributes
+  context do
+    attributes = Schema::Controls::Schema::Example.attributes
 
-      test "Each defined attribute is included in the class's attribute list" do
-        assert(attributes.length == 1)
+    test "Each defined attribute is included in the class's attribute list" do
+      assert(attributes.length == 1)
+    end
+
+    test "Name is the declared name" do
+      attribute = attributes[0]
+      assert(attribute.name == :some_attribute)
+    end
+  end
+
+  context "Not Typed" do
+    attributes = Schema::Controls::Schema::Example.attributes
+
+    context "Attribute List Entry" do
+      attribute = attributes[0]
+
+      test "Type is not declared" do
+        assert(attribute.type.nil?)
       end
 
-      context "Attribute List Entry" do
-        attribute = attributes[0]
-
-        test "Name is the declared name" do
-          assert(attribute.name == :some_attribute)
-        end
-
-        test "Type is not declared" do
-          assert(attribute.type.nil?)
-        end
-
-        test "Strict is false" do
-          refute(attribute.strict)
-        end
+      test "Strict is false" do
+        refute(attribute.strict)
       end
     end
   end
@@ -31,16 +34,8 @@ context "Class Attributes List" do
     context "Not Strict" do
       attributes = Schema::Controls::Schema::Typed::Example.attributes
 
-      test "Each defined attribute is included in the class's attribute list" do
-        assert(attributes.length == 1)
-      end
-
       context "Attribute List Entry" do
         attribute = attributes[0]
-
-        test "Name is the declared name" do
-          assert(attribute.name == :some_attribute)
-        end
 
         test "Type is the declared type" do
           assert(attribute.type == Schema::Controls::Schema::Typed::SomeType)
