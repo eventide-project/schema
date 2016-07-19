61f48baa64e060fc9b228cbba1ea6ea408333bbc
Scott Bellware <sbellware@gmail.com>
2016-07-19 00:30:57 -0500

    Case equality operator considers attributes and values only

---
+++ test/bench/schema/equality/case_equality.rb
@@ -0,0 +1,34 @@
+require_relative '../../bench_init'
+
+context "Case Equality" do
+  context "Equal" do
+    context "Class are equal and attributes and values are equal" do
+      example_1 = Schema::Controls::Schema.example
+      example_2 = Schema::Controls::Schema.example
+
+      test "Instances are equal" do
+        assert(example_1 === example_2)
+      end
+    end
+
+    context "Class are not equal and attributes and values are equal" do
+      example_1 = Schema::Controls::Schema.example
+      example_2 = Schema::Controls::Schema.other_example
+
+      test "Instances are equal" do
+        assert(example_1 === example_2)
+      end
+    end
+  end
+
+  context "Not Equal" do
+    context "Attributes and values are not equal" do
+      example_1 = Schema::Controls::Schema.example
+      example_2 = Schema::Controls::Schema.example
+
+      example_2.some_attribute = 'some other value'
+
+      refute(example_1 == example_2)
+    end
+  end
+end
