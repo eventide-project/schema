9b4d7b2c5ea0887d59da06e6b668cbec225a8e9a
Scott Bellware <sbellware@gmail.com>
2020-07-07 17:12:23 -0500

    Attribute equality is removed. It's superfluous.

--- lib/schema/schema.rb
+++ lib/schema/schema.rb
@@ -181,27 +181,4 @@ module Schema
     true
   end
   alias :eql? :==
-
-  def attributes_equal?(other, attributes=nil, print: nil)
-    attributes ||= self.class.attribute_names
-
-    print = true if print.nil?
-    print ||= false
-
-    equal = self.eql?(other, attributes, ignore_class: true)
-
-    if !equal && print
-      attributes = Array(attributes)
-
-      require 'pp'
-      puts "self: #{self.class.name}"
-      pp self.attributes.select { |k, v| attributes.include? k }
-      puts "other #{other.class.name}:"
-      pp other.attributes.select { |k, v| attributes.include? k }
-      puts "attributes:"
-      attributes.each { |a| puts a.inspect}
-    end
-
-    equal
-  end
 end
