5fc025c87662638a9cfcdb91ef3d2888f853a741
Scott Bellware <sbellware@gmail.com>
2020-07-07 12:41:32 -0500

    Difference for same and mapped attribute names

--- lib/schema/controls/attribute_differences.rb
+++ lib/schema/controls/attribute_differences.rb
@@ -24,7 +24,42 @@ module Schema
         end
 
         def self.compare_value
+          #Attribute::Value.some_other_attribute
+
           'some different value'
+        end
+      end
+
+      module Mapped
+        def self.example
+          differences = ::Schema::Compare::Difference::Attributes.new
+
+          differences.add(
+            Entry.control_attr_name,
+            Entry.control_value,
+            Entry.compare_attr_name,
+            Entry.compare_value
+          )
+
+          differences
+        end
+
+        module Entry
+          def self.control_attr_name
+            Attribute::Name.some_attribute
+          end
+
+          def self.control_value
+            Attribute::Value.some_attribute
+          end
+
+          def self.compare_attr_name
+            Attribute::Name.some_other_attribute
+          end
+
+          def self.compare_value
+            Attribute::Value.some_other_attribute
+          end
         end
       end
     end
---
+++ test/automated/compare/difference/attribute_differences/detection/same_attribute_names.rb
@@ -0,0 +1,32 @@
+require_relative '../../../../automated_init'
+
+context "Compare" do
+  context "Attributes" do
+    context "Attribute Differences" do
+      context "Detection" do
+        context "Same Attribute Names" do
+          context "Difference" do
+            differences = Schema::Controls::AttributeDifferences.example
+
+            attr_name = Schema::Controls::AttributeDifferences::Entry.attr_name
+            different = differences.different?(attr_name)
+
+            test "Detected" do
+              assert(different)
+            end
+          end
+
+          context "No Difference" do
+            differences = Schema::Compare::Difference::Attributes.new
+
+            different = differences.different?(SecureRandom.hex)
+
+            test "Not Detected" do
+              refute(different)
+            end
+          end
+        end
+      end
+    end
+  end
+end
