379f1e0a861022c7eca51cf27a0c54dfedadc40f
Scott Bellware <sbellware@gmail.com>
2020-07-07 12:15:35 -0500

    An entry is recorded for every attribute that is different

--- lib/schema/schema/compare/difference/attributes.rb
+++ lib/schema/schema/compare/difference/attributes.rb
@@ -13,7 +13,31 @@ module Schema
         end
 
         def self.compare(attr_names, control_attributes, compare_attributes)
-          # ...
+          instance = new
+
+          attr_names.each do |attr_name|
+            entry = compare_attribute(attr_name, control_attributes, compare_attributes)
+            instance.entries << entry if not entry.nil?
+          end
+
+          instance
+        end
+
+        def self.compare_attribute(attr_name, control_attributes, compare_attributes)
+          control_value = control_attributes[attr_name]
+          compare_value = compare_attributes[attr_name]
+
+          if control_value == compare_value
+            return nil
+          end
+
+          entry = Entry.new(
+            attr_name,
+            control_value,
+            compare_value
+          )
+
+          entry
         end
 
         def self.build(*args)
