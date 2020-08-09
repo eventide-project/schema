d0123c22243b2b3080520174ffdfd963c119df1b
Scott Bellware <sbellware@gmail.com>
2020-08-08 20:46:16 -0500

    Get all attributes

---
+++ test/automated/schema/attributes_list/instance/all_attributes/has_transient_attributes.rb
@@ -0,0 +1,26 @@
+require_relative '../../../../automated_init'
+
+context "Attributes List" do
+  context "Instance" do
+    context "All Attributes" do
+      context "Has Transient Attributes" do
+        example = Schema::Controls::Schema::TransientAttributes.example
+
+        attributes = example.all_attributes
+
+        attribute_names = example.class.all_attribute_names
+
+        detail "Attributes: #{attributes.inspect}"
+        detail "Attributes Names: #{attribute_names.inspect}"
+
+        attribute_names.each do |attribute|
+          context "#{attribute}" do
+            test "Included" do
+              assert(attributes.keys.include?(attribute))
+            end
+          end
+        end
+      end
+    end
+  end
+end
