945e78df4134803094e73836036850072387cf11
Aaron Jensen <aaronjensen@gmail.com>
2021-03-07 20:05:29 -0600

    Test is corrected

--- test/automated/data_structure/data_structure.rb
+++ test/automated/data_structure/data_structure.rb
@@ -8,6 +8,6 @@ context "Data Structure" do
 
   test "Can configure its dependencies" do
     example = Schema::Controls::DataStructure::ConfigureDependencies.example
-    assert(example.some_dependency = :set)
+    assert(example.some_dependency == :set)
   end
 end
